d44c5e4fa8d4141ea660475d68d6b0eb788b95f1
Horacio Capdevila <61241842+hor-5@users.noreply.github.com>
2021-09-02 02:31:02 -0300

    first commit

---
+++ src/logica/Main.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Pantalla screen = new Pantalla();
+        screen.setVisible(true);
+        screen.setLocationRelativeTo(null);
+        
+    }
+    
+}
